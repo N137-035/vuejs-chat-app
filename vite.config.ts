81447e4829cc0dcb796baee81a69e72627df3cd1
N <N137.035999@gmail.com>
2024-01-08 04:13:22 +0900

    :lipstick: Install Vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: './src/auto-imports.d.ts'
+    })
   ],
   resolve: {
     alias: {
